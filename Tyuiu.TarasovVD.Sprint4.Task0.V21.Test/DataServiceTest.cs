eea64c496d7aaf5ac112dca8ff2b3676970329fb
Владислав Тарасов <Tyuiu.TarasovVD@outlook.com>
2023-11-03 22:20:27 +0300

    Add project files.

---
+++ Tyuiu.TarasovVD.Sprint4.Task0.V21.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.TarasovVD.Sprint4.Task0.V21.Lib;
+
+namespace Tyuiu.TarasovVD.Sprint4.Task0.V21.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidGetSumOddArrEl()
+        {
+            DataService ds = new DataService();
+            int[] numsArray = { 8, 2, 4, 3, 8, 5, 6, 7, 9, 8 };
+            int res = ds.GetSumOddArrEl(numsArray);
+            int numsWaitArray = 36;
+            Assert.AreEqual(numsWaitArray, res);
+        }
+    }
+}
---
+++ Tyuiu.TarasovVD.Sprint4.Task0.V21/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.TarasovVD.Sprint4.Task0.V21.Lib;
+
+namespace Tyuiu.TarasovVD.Sprint4.Task0.V21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ                                                         *");
+            Console.WriteLine("***************************************************************************");
+            int[] array = { 8, 2, 4, 3, 8, 5, 6, 7, 9, 8 };
+            Console.WriteLine("Дан массив: { 8, 2, 4, 3, 8, 5, 6, 7, 9, 8 }");
+            Console.WriteLine();
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            int sum = ds.GetSumOddArrEl(array);
+            Console.WriteLine("Сумма четных элементов: " + sum);
+
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
